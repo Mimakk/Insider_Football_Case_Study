7e43a73c52f959142a662dfcc024392aae399195
Mina Nilay TEZER <107746643+Mimakk@users.noreply.github.com>
2025-05-24 22:16:09 +0300

    remove deprecated rand.Seed

--- simulator/match.go
+++ simulator/match.go
@@ -3,11 +3,9 @@ package simulator
 import (
 	"insider_football_case_study/models"
 	"math/rand"
-	"time"
 )
 
 func SimulateMatch(home, away models.Team) (int, int) {
-	rand.Seed(time.Now().UnixNano())
 	base := 1.5
 	homeGoals := int(home.Strength * base * rand.Float64())
 	awayGoals := int(away.Strength * base * rand.Float64())
